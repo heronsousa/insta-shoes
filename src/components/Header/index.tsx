d9242795b70223c057ec57cb8c612ec20293dc74
Heron Rodrigues <heronrs1@gmail.com>
2021-07-18 12:07:47 -0300

    Create header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,40 @@
+import './styles.css'
+import logo from '../../assets/logo.svg'
+import search from '../../assets/search.svg'
+import like from '../../assets/like.svg'
+import cart from '../../assets/cart.svg'
+
+export function Header() {
+    return (
+        <header>
+            <div className="container">
+                <img width="200px" src={logo} alt="Logo Insta Shoes" />
+                
+                <div className="tabs">
+                    <a href="/"> 
+                        <strong>home</strong> 
+                    </a>
+                    <a href="/">homem</a>
+                    <a href="/">mulher</a>
+                    <a href="/">criança</a>
+                </div>
+
+                <form>
+                    <input type="text" placeholder="Ex.: Spring blade" />
+                    <button type="submit">
+                        <img src={search} alt="Buscar" />
+                    </button>
+                </form>
+
+                <div className="actions">
+                    <button>
+                        <img src={like} alt="Favoritos" />
+                    </button>
+                    <button>
+                        <img src={cart} alt="Carrinho" />
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
+}
